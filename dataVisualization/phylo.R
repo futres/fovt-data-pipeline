528459e47de7bc9e021853150798e261428d928e
Meghan Balk <meghan.balk@gmail.com>
2021-04-22 11:52:32 -0700

    adding tools for data visualizatin

---
+++ dataVisualization/phylo.R
@@ -0,0 +1,9 @@
+## pie chart of proportion of groups of taxa
+
+## pie chart of proportion of traits
+
+## simple correlations of BM v HBL
+
+## bar graph of traits (e.g., body size, with % of HBL, BM, TL, etc.)
+
+## phylo of major clades and number of traits
